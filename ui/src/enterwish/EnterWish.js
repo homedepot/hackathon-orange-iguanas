4e5d44c01cace14d940d096c87e2883ed4bc0e25
Tan Lui <effishing@gmail.com>
2019-06-14 15:16:08 -0400

    Adds css to enter wish

--- ui/src/enterwish/EnterWish.js
+++ ui/src/enterwish/EnterWish.js
@@ -1,7 +1,66 @@
 import React, { Component  } from "react";
+import './EnterWish.css'
 
 export default class EnterWish extends Component {
+ 
+
+  state = {
+    hometown: null,
+    illness: null,
+    myWish: null
+  }
+  handleSubmit = (event) => {
+    alert('...wish submitted: ' + this.state.myWish);
+    event.preventDefault();
+  }
+
+  handleHometownChange = (event) => {
+    this.setState({hometown: event.target.value});
+  }
+  
+  handleIllnessChange = (event) => {
+    this.setState({illness: event.target.value});
+  }
+
+  handleWishChange = (event) => {
+    this.setState({wish: event.target.value});
+  }
+
+  handleWishDetailsChange = (event) => {
+    this.setState({wishDetails: event.target.value});
+  }
+
   render(){
-    return <div>Enter Wish</div>
+    const { name } = this.props;
+    return (
+      <div className="enter-wish" >  
+        <div className="child-name">Hello, {`${name}`}</div> 
+        <form onSubmit={this.handleSubmit}>  
+          <label>
+            {`${name}'s hometown is..`}
+            <input type="text" value={this.state.hometown} onChange={this.handleHometownChange}/>
+          </label><br/>
+          <label>
+            {`${name}'s illness..`}
+            <input type="text" value={this.state.illness} onChange={this.handleIllnessChange}/>
+          </label>
+          <br/>
+          <label>
+            {`${name}'s wish is..`}
+            <input type="text" value={this.state.myWish} onChange={this.handleWishChange}/>
+          </label>
+          <br/>
+          <label>
+            <div> 
+              {`Tell us more about ${name}'s wish..`} 
+            </div>
+            <textarea rows="6" cols="50" value={this.state.wishDetails} onChange={this.handleWishDetailsChange} />  
+          </label>
+          <div> 
+          <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
