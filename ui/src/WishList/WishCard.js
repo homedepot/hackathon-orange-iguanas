3211e20d6bac233ad0b02e0139c1db96059a0402
Claire Moss <Claire_Moss@homedepot.com>
2019-06-14 18:11:11 -0400

    Updated wish card to show icons

--- ui/src/WishList/WishCard.js
+++ ui/src/WishList/WishCard.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import * as constants from '../constants'
 import './WishCard.css'
+import Image from '../Image'
 
 export default class WishCard extends Component {
   render() {
@@ -14,18 +15,28 @@ export default class WishCard extends Component {
       sponsor
     } = this.props.data
     let verb
+    let imgclass
+    let imgsrc
     switch (wishType) {
       case constants.WISH_TYPE_GO:
         verb = ' go to ';
+        imgclass = 'icon';
+        imgsrc = 'goSomewhere';
         break;
         case constants.WISH_TYPE_MEET:
         verb = ' meet ';
+        imgclass = 'icon';
+        imgsrc = 'meetSomeone';
         break;
         case constants.WISH_TYPE_SEE:
         verb = ' see ';
+        imgclass = 'icon';
+        imgsrc = 'seeSomething';
         break;
         case constants.WISH_TYPE_BE:
         verb = ' be ';
+        imgclass = 'icon';
+        imgsrc = 'beSomeone';
         break;
       default:
         break;
@@ -33,11 +44,18 @@ export default class WishCard extends Component {
     return (
       <Fragment>
         <div className="card">
-          <p>
+        <p>
+        <Image className={imgclass} src={imgsrc}/>
+        <Image className={imgclass} src={imgsrc}/>
+            <span class="verticalLine">
             <b>{firstName}</b> - Age {age} from {hometown}, GA
-          </p>
-          <p>
+            </span>
+          <span class="verticalLine">
             {firstName} wishes to {verb} {wishDetail}
+            </span>
+        <Image className={imgclass} src={imgsrc}/>
+        <Image className={imgclass} src={imgsrc}/>
+        <Image src="seeMore"/>
         </p>
         </div>
 
