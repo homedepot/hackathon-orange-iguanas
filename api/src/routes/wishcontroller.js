822137370ed9349b0cddb1a82c765de2b580b18e
Bhargava Muddaluru <bhargava_k_muddaluru@homedepot.com>
2019-06-14 15:58:51 -0400

    added the date range and findbyid endpoint to pull the wishes

--- api/src/routes/wishcontroller.js
+++ api/src/routes/wishcontroller.js
@@ -16,11 +16,34 @@ router.get('/getwishes', function(req, res) {
     })
   })
 
+router.get('/findbyid/:id', function(req, res) {
+    Wish.findById(req.params.id).then(wish => {
+        if(!wish) {
+            return res.status(200).send({
+                message: "No wishes found for given id"
+            })
+        }
+        res.status(200).json(wish)
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error finding wish."
+        })
+    })
+  })
+
 router.get('/findwish/:value', function(req, res) {
-    const query = {$or: [
-        {firstName: {$regex: req.params.value, $options: 'i'}},
-        {hometown: {$regex: req.params.value, $options: 'i'}}
-    ]}
+    var inputValue = req.params.value
+    var dateformat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/
+    var query = {}
+    if (inputValue && inputValue.match(dateformat)) {
+        const dayAfter = new Date(inputValue).getTime() + 24 * 60 * 59 * 1000;
+        query =  {$and: [ {date: {$gte: new Date(inputValue)}}, {date: {$lte: new Date(dayAfter)}}]}
+    } else {
+        query = {$or: [
+            {firstName: {$regex: inputValue, $options: 'i'}},
+            {hometown: {$regex: inputValue, $options: 'i'}}
+        ]}
+    }
     Wish.find(query).then(wish => {
         if(wish && wish.length === 0) {
             return res.status(200).send({
@@ -78,6 +101,32 @@ router.delete('/deletewish/:id', function(req, res) {
         }
         return res.status(500).send({
             message: "Could not delete wish with id " + req.params.id
+        })
+    })
+  })
+
+router.get('/findwish/:dateFrom/to/:dateTo', function(req, res) {
+    var inputDateFrom = req.params.dateFrom
+    var inputDateTo = req.params.dateTo
+    var dateformat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/
+    var query = {}
+    if (inputDateFrom && inputDateTo && inputDateFrom.match(dateformat) && inputDateTo.match(dateformat)) {
+        query =  {$and: [ {date: {$gte: new Date(inputDateFrom)}}, {date: {$lte: new Date(inputDateTo)}}]}
+    } else {
+        return res.status(400).send({
+           message: "Input item missing or please enter valid dates in format yyyy-mm-dd"
+       })
+    }
+    Wish.find(query).then(wish => {
+        if(wish && wish.length === 0) {
+            return res.status(200).send({
+                message: "No wishes found for the given date range"
+            })
+        }
+        res.status(200).json(wish)
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error searching wishes."
         })
     })
   })
