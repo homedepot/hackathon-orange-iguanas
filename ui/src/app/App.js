5df4200615a8bbe74407237c524f69f7d4e80ad0
Eshan Bairagi <eshan_bairagi1@homedepot.com>
2019-06-14 12:54:53 -0400

    add index files

--- ui/src/app/App.js
+++ ui/src/app/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
-import Login from '../login/Login'
-import Landing from '../landing/Landing'
-import WishList from '../wishList/WishList'
+import Login from '../login'
+import Landing from '../landing'
+import WishList from '../wishList'
 import WatchAuth from '../auth/WatchAuth'
-import EnterWish from '../enterWish/EnterWish'
+import EnterWish from '../enterWish'
 
 function App() {
   return (
