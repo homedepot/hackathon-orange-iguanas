e7be4630c51b834af5e0ef149f27f74f1f6afbfb
Eshan Bairagi <eshan_bairagi1@homedepot.com>
2019-06-14 17:35:47 -0400

    animations

--- ui/src/landing/Landing.js
+++ ui/src/landing/Landing.js
@@ -2,49 +2,87 @@ import React, { Component } from 'react'
 import ImageButton from '../Image/ImageButton'
 import Image from '../Image'
 import './Landing.css'
-import TweenOne from 'rc-tween-one'
+import Spin from 'react-reveal/Spin'
+import LightSpeed from 'react-reveal/LightSpeed'
 
 export default class Landing extends Component {
   state = {
     name: '',
     age: undefined,
+    animation: {
+      d: 'M60,10L60,90L140,90L140,10Z',
+      yoyo: true,
+      repeat: -1,
+      duration: 1000
+    }
   }
 
   handleSubmit = () => {
     return true
   }
-  handleNameChange = () => {
-  
-  }
-  handleAgeChange = () => {
-  
-  }
+  handleNameChange = () => {}
+  handleAgeChange = () => {}
 
   render() {
     return (
       <div className="landing">
-        <div className="landing-title">Hello!!! <Image className="galaxy-image" src="logoGalaxy"/></div>
+        <div className="landing-title">
+          Hello!!!{' '}
+          <LightSpeed forever={true}>
+
+              <Image className="galaxy-image" src="logoGalaxy" />
+
+          </LightSpeed>
+        </div>
         <form className="form" onSubmit={this.handleSubmit}>
           <label>
             My Name is:
-            <input className="text-input" type="text" 
-            value={this.state.name} onChange={this.handleNameChange}
-            placeholder="enter your name"/>
+            <input
+              className="text-input"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              placeholder="enter your name"
+            />
           </label>
           <label>
             And my age is
-            <input type="text" className="text-input" 
-            value={this.state.age} onChange={this.handleAgeChange}
-            placeholder="your age" />
+            <input
+              type="text"
+              className="text-input"
+              value={this.state.age}
+              onChange={this.handleAgeChange}
+              placeholder="your age"
+            />
             years old!
           </label>
         </form>
         <div className="wish-to">I wish to:</div>
         <div className="image-buttons">
-          <ImageButton src='goSomewhere' txt1="GO" txt2="Somewhere" navigateTo="/enter-wish" />
-          <ImageButton src='meetSomeone' txt1="MEET" txt2="Someone" navigateTo="/enter-wish"/>
-          <ImageButton src='beSomeone' txt1="BE" txt2="Someone" navigateTo="/enter-wish"/>
-          <ImageButton src='seeSomething' txt1="SEE" txt2="Something" navigateTo="/enter-wish"/>
+          <ImageButton
+            src="goSomewhere"
+            txt1="GO"
+            txt2="Somewhere"
+            navigateTo="/enter-wish"
+          />
+          <ImageButton
+            src="meetSomeone"
+            txt1="MEET"
+            txt2="Someone"
+            navigateTo="/enter-wish"
+          />
+          <ImageButton
+            src="beSomeone"
+            txt1="BE"
+            txt2="Someone"
+            navigateTo="/enter-wish"
+          />
+          <ImageButton
+            src="seeSomething"
+            txt1="SEE"
+            txt2="Something"
+            navigateTo="/enter-wish"
+          />
         </div>
       </div>
     )
