32ebd2b7782b02d711b58cb81124f7fae5b80806
Claire Moss <Claire_Moss@homedepot.com>
2019-06-14 23:39:12 -0400

    Added text associated with Wish Card images

--- ui/src/WishList/WishCard.js
+++ ui/src/WishList/WishCard.js
@@ -17,32 +17,42 @@ export default class WishCard extends Component {
     let verb
     let imgprofile
     let imgsponsor
+    let sponsortext
     let imgclass = 'icon'
     let imgsrc
+    let imgtext
     switch (wishType) {
       case constants.WISH_TYPE_GO:
         verb = ' go to ';
         imgprofile = 'monthMagenta';
         imgsponsor = 'thd';
+        sponsortext = "The Home Depot";
         imgsrc = 'goSomewhere';
+        imgtext = 'To Go';
         break;
         case constants.WISH_TYPE_MEET:
         verb = ' meet ';
         imgprofile = 'monthBlue';
         imgsponsor = 'thd';
+        sponsortext = "The Home Depot";
         imgsrc = 'meetSomeone';
+        imgtext = 'To Meet';
         break;
         case constants.WISH_TYPE_SEE:
         verb = ' see ';
         imgprofile = 'monthMagenta';
         imgsponsor = 'needSponsor';
+        sponsortext = "Need Sponsor";
         imgsrc = 'seeSomething';
+        imgtext = 'To See';
         break;
         case constants.WISH_TYPE_BE:
         verb = ' be ';
         imgprofile = 'monthBlue';
         imgsponsor = 'needSponsor';
+        sponsortext = "Need Sponsor";
         imgsrc = 'beSomeone';
+        imgtext = 'To Be';
         break;
       default:
         break;
@@ -50,21 +60,26 @@ export default class WishCard extends Component {
     return (
       <Fragment>
         <div className="card">
-        <p>
-        <Image className={imgclass} src={imgprofile}/>
-        <Image className={imgclass} src={imgsrc}/>
-            <span class="verticalLine">
+          <p>
+          <Image className={imgclass} src={imgprofile}/>
+          <Image className={imgclass} src={imgsrc}/>
+          
+          <span>
             <b>{firstName}</b> - Age {age} from {hometown}, GA
-            </span>
-          <span class="verticalLine">
+          </span>
+          <span>
             {firstName} wishes to {verb} {wishDetail}
-            </span>
-        <Image className={imgclass} src={imgsponsor}/>
-        <Image className={imgclass} src={imgsrc}/>
-        <Image src="seeMore"/>
-        </p>
-        <p>{date}</p>
-        <p>{sponsor}</p>
+          </span>
+          <span> </span>
+        
+          <label class="verticalLine">{sponsortext}</label>
+          <Image className={imgclass} src={imgsponsor}/>
+          
+          <label class="verticalLine">{imgtext}</label>
+          <Image className={imgclass} src={imgsrc}/>
+          
+          <Image src="seeMore"/>
+          </p>
         </div>
       </Fragment>
     )
